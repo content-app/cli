30ecf1cc79fc906cc4a785e74be45fef5776178f
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-03-25 22:04:20 +0000

    Implement load module

--- bin/content-app.js
+++ bin/content-app.js
@@ -5,6 +5,7 @@ const readline = require('readline');
 const contentful = require('contentful-management');
 const contentApp = require('@content-app/core').default;
 const program = new Command();
+const path = require('path');
 
 
 program
@@ -34,6 +35,48 @@ program
         });
         } catch (error) {
           console.error(console.error(error));
+        }
+        
+      } else {
+        console.log('Aborted.');
+      }
+      rl.close();
+    });
+  });
+
+  program
+  .command('load-content-module <contentModule>')
+  .description('Loads a content module from a JavaScript file')
+  .requiredOption('-a, --accessToken <accessToken>', 'Access token')
+  .requiredOption('-s, --spaceId <spaceId>', 'Space ID')
+  .option('-e, --environment <environment>', 'Environment')
+  .action(async (contentModule, cmd) => {
+    const { accessToken, spaceId, environment } = cmd;
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(`Are you sure you want to load the content module ${contentModule} into accessToken: ${accessToken}, spaceId: ${spaceId}, environment: ${environment || 'default'}? (y/n) `, async (answer) => {
+      if (answer.toLowerCase() === 'y') {
+        const modulePath = path.join(process.cwd(), 'node_modules', `@content-app/content-module_${contentModule}/install.js`);
+        console.log(modulePath)
+        const contentModuleInstall = require(modulePath);
+        
+        const client = contentful.createClient({
+          accessToken: accessToken,
+        });
+
+        const space = await client.getSpace(spaceId);
+        const environment = await space.getEnvironment(environment || 'master' || 'main');
+        
+        try {
+          await contentModuleInstall({
+            client: client,
+            space: space,
+            environment: environment,
+          })
+        } catch (error) {
+          console.error(error);
         }
         
       } else {
